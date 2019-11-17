65c376edd9579f8a4555350e4bd5f629d8ea1723
Vuong Nguyen <vuongnq14798@gmail.com>
2019-11-17 22:20:36 +0700

    Task#2 Create Base

---
+++ app/src/main/java/com/vuongnq1407/mediaplayer/ui/home/HomeFragment.kt
@@ -0,0 +1,16 @@
+package com.vuongnq1407.mediaplayer.ui.home
+
+import android.os.Bundle
+import com.vuongnq1407.mediaplayer.R
+import com.vuongnq1407.mediaplayer.base.BaseFragment
+
+class HomeFragment : BaseFragment(), HomeContract.View {
+
+    override val layoutResId: Int = R.layout.fragment_home
+
+    override fun initData(savedInstanceState: Bundle?) {
+    }
+
+    override fun initComponents() {
+    }
+}
